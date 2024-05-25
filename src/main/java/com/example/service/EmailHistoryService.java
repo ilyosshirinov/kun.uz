633a8dca831dd76b79b25764fbd6c6f10cc3a2e1
ilyosshirinov <shirinovilyos6965@gmail.com>
2024-05-25 16:59:24 +0500

    13. EmailHistory Hammasi tayyor     id, message, email, created_data     1. Create EmailHistory when email is send using application. (No need create api)     2. Get EmailHistory by email             (id, email,message,created_date)     3. Get EmailHistory  by given date             (id, email,message,created_date)     4. Pagination (ADMIN)             (id, email,message,created_date)

--- src/main/java/com/example/service/EmailHistoryService.java
+++ src/main/java/com/example/service/EmailHistoryService.java
@@ -1,12 +1,20 @@
 package com.example.service;
 
+import com.example.dto.emailHistory.EmailHistoryDto;
 import com.example.entity.EmailHistoryEntity;
 import com.example.exp.AppBadException;
 import com.example.repository.EmailHistoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -49,5 +57,61 @@ public class EmailHistoryService {
         if (entity.getCreatedDate().plusDays(1).isBefore(LocalDateTime.now())) {
             throw new AppBadException("Confirmation time expired");
         }
+    }
+
+    public List<EmailHistoryDto> getAllEmailService(String email) {
+        List<EmailHistoryEntity> entity = emailHistoryRepository.getAllByEmail(email);
+        if (entity.size() == 0) {
+            throw new AppBadException(email + " mavjud emas!");
+        }
+        List<EmailHistoryDto> list = new ArrayList<>();
+        for (EmailHistoryEntity emailHistoryEntity : entity) {
+            EmailHistoryDto dto = new EmailHistoryDto();
+            dto.setId(emailHistoryEntity.getId());
+            dto.setEmail(emailHistoryEntity.getEmail());
+            dto.setMessage(emailHistoryEntity.getMessage());
+            dto.setCreatedDate(emailHistoryEntity.getCreatedDate());
+            list.add(dto);
+        }
+        return list;
+    }
+
+    public List<EmailHistoryDto> getByCreatedDateEmailService(LocalDate date) {
+        LocalDate from = date;
+        LocalDate to = from.plusDays(1);
+        List<EmailHistoryEntity> entity = emailHistoryRepository.getAllCreatedDate(from, to);
+        if (entity.size() == 0) {
+            throw new AppBadException(date + " kunda Email mavjud emas!");
+        }
+        List<EmailHistoryDto> list = new ArrayList<>();
+        for (EmailHistoryEntity emailHistoryEntity : entity) {
+            EmailHistoryDto dto = new EmailHistoryDto();
+            dto.setId(emailHistoryEntity.getId());
+            dto.setEmail(emailHistoryEntity.getEmail());
+            dto.setMessage(emailHistoryEntity.getMessage());
+            dto.setCreatedDate(emailHistoryEntity.getCreatedDate());
+            list.add(dto);
+        }
+        return list;
+    }
+
+    public Page<EmailHistoryDto> getAllPageEmailService(Integer page, Integer size) {
+        Pageable pageable = PageRequest.of(page, size);
+        Page<EmailHistoryEntity> entityPage = emailHistoryRepository.findAll(pageable);
+        return toDtoEmail(entityPage, pageable); // todo kurish kerak
+    }
+
+    public Page<EmailHistoryDto> toDtoEmail(Page<EmailHistoryEntity> entityPage, Pageable pageable) {
+        List<EmailHistoryDto> list = new ArrayList<>();
+        for (EmailHistoryEntity emailHistoryEntity : entityPage) {
+            EmailHistoryDto dto = new EmailHistoryDto();
+            dto.setId(emailHistoryEntity.getId());
+            dto.setEmail(emailHistoryEntity.getEmail());
+            dto.setCreatedDate(emailHistoryEntity.getCreatedDate());
+            dto.setMessage(emailHistoryEntity.getMessage());
+            list.add(dto);
+        }
+        Long totalElements = entityPage.getTotalElements();
+        return new PageImpl<>(list, pageable, totalElements);
     }
 }
